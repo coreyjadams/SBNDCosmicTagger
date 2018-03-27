3630b699249514d7e3231c9292e24a3da950d0a4
coreyjadams <coreyadams@fas.harvard.edu>
2018-03-27 16:31:07 -0500

    Reconfigure for multiplane use, single plane should still work

--- uresnet.py
+++ uresnet.py
@@ -31,6 +31,7 @@ class uresnet(object):
             'MINIBATCH_SIZE',
             'SAVE_ITERATION',
             'NUM_LABELS',
+            'NPLANES',
             'N_INITIAL_FILTERS',
             'NETWORK_DEPTH',
             'RESIDUAL_BLOCKS_PER_LAYER',
@@ -63,17 +64,31 @@ class uresnet(object):
         self._input_image  = tf.placeholder(tf.float32, dims, name="input_image")
         self._input_labels = tf.placeholder(tf.int64, dims, name="input_image")
 
-        # Prune off the last filter of the labels:
-        labels = tf.squeeze(self._input_labels, axis=-1)
 
+        labels = tf.split(self._input_labels, [1]*self._params['NPLANES'], -1)
         if self._params['BALANCE_LOSS']:
             self._input_weights = tf.placeholder(tf.float32, dims, name="input_image")
-            weights = tf.squeeze(self._input_weights, axis=-1)
+            weights = tf.split(self._input_weights, [1]*self._params['NPLANES'], -1)
+
+        # Prune off the last filter of the labels and weights:
+        for p in xrange(len(labels)):
+            labels[p] = tf.squeeze(labels[p], axis=-1)
+
+        for p in xrange(len(weights)):
+            weights[p] = tf.squeeze(weights[p], axis=-1)
 
 
-        logits = self._build_network(self._input_image)
-        self._softmax = tf.nn.softmax(logits)
-        self._predicted_labels = tf.argmax(logits, axis=-1)
+        logits_by_plane = self._build_network(self._input_image)
+        # for p in xrange(len(logits_by_plane)):
+        #     print "logits_by_plane[{0}].get_shape(): ".format(p) + str(logits_by_plane[p].get_shape())
+        self._softmax = [tf.nn.softmax(logits) for logits in logits_by_plane ]
+        self._predicted_labels = [ tf.argmax(logits, axis=-1) for logits in logits_by_plane ]
+
+        # for p in xrange(len(self._softmax)):
+        #     print "self._softmax[{0}].get_shape(): ".format(p) + str(self._softmax[p].get_shape())
+        # for p in xrange(len(self._predicted_labels)):
+        #     print "self._predicted_labels[{0}].get_shape(): ".format(p) + str(self._predicted_labels[p].get_shape())
+
 
         # Keep a list of trainable variables for minibatching:
         with tf.variable_scope('gradient_accumulation'):
@@ -83,32 +98,53 @@ class uresnet(object):
 
         # Accuracy calculations:
         with tf.name_scope('accuracy'):
-            self._total_accuracy   = tf.reduce_mean(tf.cast(tf.equal(self._predicted_labels,
-                                                               labels), tf.float32))
-            # Find the non zero labels:
-            non_zero_indices = tf.not_equal(labels, tf.constant(0, labels.dtype))
+            self._total_accuracy   = [ [] for i in range(self._params['NPLANES']) ]
+            self._non_bkg_accuracy = [ [] for i in range(self._params['NPLANES']) ]
+            for p in xrange(len(self._predicted_labels)):
+                self._total_accuracy[p] = tf.reduce_mean(
+                    tf.cast(tf.equal(self._predicted_labels[p],
+                        labels[p]), tf.float32))
+                # Find the non zero labels:
+                non_zero_indices = tf.not_equal(labels[p], tf.constant(0, labels[p].dtype))
 
-            non_zero_logits = tf.boolean_mask(self._predicted_labels, non_zero_indices)
-            non_zero_labels = tf.boolean_mask(labels, non_zero_indices)
+                non_zero_logits = tf.boolean_mask(self._predicted_labels[p], non_zero_indices)
+                non_zero_labels = tf.boolean_mask(labels[p], non_zero_indices)
 
-            self._non_bkg_accuracy = tf.reduce_mean(tf.cast(tf.equal(non_zero_logits, non_zero_labels), tf.float32))
+                self._non_bkg_accuracy[p] = tf.reduce_mean(tf.cast(tf.equal(non_zero_logits, non_zero_labels), tf.float32))
+
+                # Add the accuracies to the summary:
+                tf.summary.scalar("Total_Accuracy_plane{0}".format(p),
+                    self._total_accuracy[p])
+                tf.summary.scalar("Non_Background_Accuracy_plane{0}".format(p),
+                    self._non_bkg_accuracy[p])
+
+            #Compute the total accuracy and non background accuracy for all planes:
+            self._all_plane_accuracy = tf.reduce_mean(self._total_accuracy)
+            self._all_plane_non_bkg_accuracy = tf.reduce_mean(self._non_bkg_accuracy)
 
             # Add the accuracies to the summary:
-            tf.summary.scalar("Total_Accuracy", self._total_accuracy)
-            tf.summary.scalar("Non_Background_Accuracy", self._non_bkg_accuracy)
+            tf.summary.scalar("All_Plane_Total_Accuracy", self._all_plane_accuracy)
+            tf.summary.scalar("All_Plane_Non_Background_Accuracy", self._all_plane_non_bkg_accuracy)
+
 
         # Loss calculations:
         with tf.name_scope('cross_entropy'):
+            self._loss_by_plane = [ [] for i in range(self._params['NPLANES']) ]
+            for p in xrange(len(logits_by_plane)):
 
-            # Unreduced loss, shape [BATCH, L, W]
-            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+                # Unreduced loss, shape [BATCH, L, W]
+                losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels[p],
+                    logits=logits_by_plane[p])
 
-            if self._params['BALANCE_LOSS']:
-                losses = tf.multiply(losses, weights)
+                if self._params['BALANCE_LOSS']:
+                    losses = tf.multiply(losses, weights)
 
-            self._loss = tf.reduce_mean(tf.reduce_sum(losses, axis=[1,2]))
+                self._loss_by_plane[p] = tf.reduce_mean(tf.reduce_sum(losses, axis=[1,2]))
 
-            # Add the loss to the summary:
+                # Add the loss to the summary:
+                tf.summary.scalar("Total_Loss_plane{0}".format(p), self._loss_by_plane[p])
+
+            self._loss = tf.reduce_mean(self._loss_by_plane)
             tf.summary.scalar("Total_Loss", self._loss)
 
 
@@ -135,23 +171,16 @@ class uresnet(object):
 
         # Snapshotting:
         with tf.name_scope('snapshot'):
+            for p in xrange(len(logits_by_plane)):
 
-            target_img = tf.cast(tf.reshape(labels, labels.get_shape().as_list() + [1,]), tf.float32)
-            target_img = tf.image.grayscale_to_rgb(target_img)
-            tf.summary.image('labels', target_img,max_outputs=10)
+                target_img = tf.cast(tf.reshape(labels[p], labels[p].get_shape().as_list() + [1,]), tf.float32)
+                target_img = tf.image.grayscale_to_rgb(target_img)
+                tf.summary.image('labels_plane{0}'.format(p), target_img,max_outputs=10)
 
-            output_img = tf.cast(tf.reshape(self._predicted_labels, labels.get_shape().as_list() + [1,]), tf.float32)
-            output_img = tf.image.grayscale_to_rgb(output_img)
-            tf.summary.image('logits', output_img,max_outputs=10)
-            # for label in xrange(len(self._params['LABEL_NAMES'])):
-            #     target_img = tf.cast(
-            #         tf.equal(labels, tf.constant(label, labels.dtype)), tf.float32)
-            #     target_img = tf.reshape(target_img, target_img.get_shape().as_list() + [1,])
-            #     output_img = tf.cast(
-            #         tf.equal(self._predicted_labels, tf.constant(label, labels.dtype)), tf.float32)
-            #     output_img = tf.reshape(output_img, output_img.get_shape().as_list() + [1,])
-            #     tf.summary.image('{}_labels'.format(self._params['LABEL_NAMES'][label]), , max_outputs=1)
-            #     tf.summary.image('{}_logits'.format(self._params['LABEL_NAMES'][label]), tf.reshape(output_img, output_img.get_shape().as_list() + [1,]), max_outputs=1)
+                output_img = tf.cast(tf.reshape(self._predicted_labels[p], labels[p].get_shape().as_list() + [1,]), tf.float32)
+                output_img = tf.image.grayscale_to_rgb(output_img)
+                tf.summary.image('logits_plane{0}'.format(p), output_img,max_outputs=10)
+
 
         # Merge the summaries:
         self._merged_summary = tf.summary.merge_all()
@@ -213,7 +242,7 @@ class uresnet(object):
         ops = [self._accum_gradients]
         doc = ['']
         # classification
-        ops += [self._loss, self._total_accuracy, self._non_bkg_accuracy]
+        ops += [self._loss, self._all_plane_accuracy, self._all_plane_non_bkg_accuracy]
         doc += ['loss', 'acc. all', 'acc. nonzero']
 
         return sess.run(ops, feed_dict = feed_dict ), doc
@@ -246,92 +275,152 @@ class uresnet(object):
     def _build_network(self, input_placeholder):
 
         x = input_placeholder
-        # Initial convolution to get to the correct number of filters:
-        x = tf.layers.conv2d(x, self._params['N_INITIAL_FILTERS'],
-                             kernel_size=[7, 7],
-                             strides=[1, 1],
-                             padding='same',
-                             use_bias=False,
-                             trainable=self._params['TRAINING'],
-                             name="Conv2DInitial",
-                             reuse=None)
 
-        # ReLU:
-        x = tf.nn.relu(x)
+        # We break up the intial filters into parallel U ResNets
+        # The filters are concatenated at the deepest level
+        # And then they are split again into the parallel chains
+
+        # print x.get_shape()
+        n_planes = self._params['NPLANES']
+
+        x = tf.split(x, n_planes*[1], -1)
+        # for p in range(len(x)):
+        #     print x[p].get_shape()
+
+        # Initial convolution to get to the correct number of filters:
+        for p in range(len(x)):
+            x[p] = tf.layers.conv2d(x[p], self._params['N_INITIAL_FILTERS'],
+                                    kernel_size=[7, 7],
+                                    strides=[1, 1],
+                                    padding='same',
+                                    use_bias=False,
+                                    trainable=self._params['TRAINING'],
+                                    name="Conv2DInitial_plane{0}".format(p),
+                                    reuse=None)
+
+            # ReLU:
+            x[p] = tf.nn.relu(x[p])
+
+        # for p in range(len(x)):
+        #     print x[p].get_shape()
+
+
+
 
         # Need to keep track of the outputs of the residual blocks before downsampling, to feed
         # On the upsampling side
 
-        network_filters = []
+        network_filters = [[] for p in range(len(x))]
 
         # Begin the process of residual blocks and downsampling:
-        for i in xrange(self._params['NETWORK_DEPTH']):
+        for p in xrange(len(x)):
+            for i in xrange(self._params['NETWORK_DEPTH']):
 
-            for j in xrange(self._params['RESIDUAL_BLOCKS_PER_LAYER']):
-                x = residual_block(x, self._params['TRAINING'],
-                                   batch_norm=True,
-                                   name="resblock_down_{0}_{1}".format(i, j))
+                for j in xrange(self._params['RESIDUAL_BLOCKS_PER_LAYER']):
+                    x[p] = residual_block(x[p], self._params['TRAINING'],
+                                          batch_norm=True,
+                                          name="resblock_down_plane{0}_{1}_{2}".format(p, i, j))
 
-            network_filters.append(x)
-            x = downsample_block(x, self._params['TRAINING'],
-                                batch_norm=True,
-                                name="downsample_{0}".format(i))
+                network_filters[p].append(x[p])
+                x[p] = downsample_block(x[p], self._params['TRAINING'],
+                                        batch_norm=True,
+                                        name="downsample_plane{0}_{1}".format(p,i))
 
+                # print "Plane {p}, layer {i}: x[{p}].get_shape(): {s}".format(
+                #     p=p, i=i, s=x[p].get_shape())
+
+        # print "Reached the deepest layer."
+
+        # Here, concatenate all the planes together before the residual block:
+        x = tf.concat(x, axis=-1)
+        # print "Shape after concat: " + str(x.get_shape())
 
         # At the bottom, do another residual block:
         for j in xrange(self._params['RESIDUAL_BLOCKS_PER_LAYER']):
             x = residual_block(x, self._params['TRAINING'],
                 batch_norm=True, name="deepest_block_{0}".format(j))
 
+        # print "Shape after deepest block: " + str(x.get_shape())
+
+        # Need to split the network back into n_planes
+        # The deepest block doesn't change the shape, so
+        # it's easy to split:
+        x = tf.split(x, n_planes, -1)
+
+        # for p in range(len(x)):
+        #     print x[p].get_shape()
+
+        # print "Upsampling now."
+
 
         # Come back up the network:
-        for i in xrange(self._params['NETWORK_DEPTH']-1, -1, -1):
+        for p in xrange(len(x)):
+            for i in xrange(self._params['NETWORK_DEPTH']-1, -1, -1):
 
-            # How many filters to return from upsampling?
-            n_filters = network_filters[-1].get_shape().as_list()[-1]
+                # print "Up start, Plane {p}, layer {i}: x[{p}].get_shape(): {s}".format(
+                #     p=p, i=i, s=x[p].get_shape())
 
-            # Upsample:
-            x = upsample_block(x, self._params['TRAINING'], batch_norm=False,n_output_filters=n_filters, name="upsample_{}".format(i))
+                # How many filters to return from upsampling?
+                n_filters = network_filters[p][-1].get_shape().as_list()[-1]
+
+                # Upsample:
+                x[p] = upsample_block(x[p],
+                                      self._params['TRAINING'],
+                                      batch_norm=True,
+                                      n_output_filters=n_filters,
+                                      name="upsample_plane{0}_{1}".format(p,i))
 
 
-            x = tf.concat([x, network_filters[-1]], axis=-1, name='up_concat_{}'.format(i))
+                x[p] = tf.concat([x[p], network_filters[p][-1]],
+                                  axis=-1, name='up_concat_plane{0}_{1}'.format(p,i))
 
-            # Remove the recently concated filters:
-            network_filters.pop()
-            with tf.variable_scope("bottleneck_{}".format(i)):
-                # Include a bottleneck to reduce the number of filters after upsampling:
-                x = tf.layers.conv2d(x,
-                                     n_filters,
-                                     kernel_size=[1,1],
-                                     strides=[1,1],
-                                     padding='same',
-                                     activation=None,
-                                     use_bias=False,
-                                     trainable=self._params['TRAINING'],
-                                     name="BottleneckUpsample_{}".format(i))
+                # Remove the recently concated filters:
+                network_filters[p].pop()
+                with tf.variable_scope("bottleneck_plane{0}_{1}".format(p,i)):
+                    # Include a bottleneck to reduce the number of filters after upsampling:
+                    x[p] = tf.layers.conv2d(x[p],
+                                            n_filters,
+                                            kernel_size=[1,1],
+                                            strides=[1,1],
+                                            padding='same',
+                                            activation=None,
+                                            use_bias=False,
+                                            trainable=self._params['TRAINING'],
+                                            name="BottleneckUpsample_plane{0}_{1}".format(p,i))
 
-                x = tf.nn.relu(x)
+                    x[p] = tf.nn.relu(x[p])
 
-            # Residual
-            for j in xrange(self._params['RESIDUAL_BLOCKS_PER_LAYER']):
-                x = residual_block(x, self._params['TRAINING'],
-                                   batch_norm=False,
-                                   name="resblock_up_{0}_{1}".format(i, j))
+                # Residual
+                for j in xrange(self._params['RESIDUAL_BLOCKS_PER_LAYER']):
+                    x[p] = residual_block(x[p], self._params['TRAINING'],
+                                          batch_norm=True,
+                                          name="resblock_up_plane{0}_{1}_{2}".format(p,i, j))
 
-        x = residual_block(x, self._params['TRAINING'], batch_norm = False, name="FinalResidualBlock")
+                # print "Up end, Plane {p}, layer {i}: x[{p}].get_shape(): {s}".format(
+                #     p=p, i=i, s=x[p].get_shape())
 
-        # At this point, we ought to have a network that has the same shape as the initial input, but with more filters.
-        # We can use a bottleneck to map it onto the right dimensions:
-        x = tf.layers.conv2d(x,
-                             self._params['NUM_LABELS'],
-                             kernel_size=[7,7],
-                             strides=[1, 1],
-                             padding='same',
-                             activation=None,
-                             use_bias=False,
-                             trainable=self._params['TRAINING'],
-                             name="BottleneckConv2D",)
 
+        for p in xrange(len(x)):
+
+
+            x[p] = residual_block(x[p],
+                    self._params['TRAINING'],
+                    batch_norm = True,
+                    name="FinalResidualBlock_plane{0}".format(p))
+
+            # At this point, we ought to have a network that has the same shape as the initial input, but with more filters.
+            # We can use a bottleneck to map it onto the right dimensions:
+            x[p] = tf.layers.conv2d(x[p],
+                                 self._params['NUM_LABELS'],
+                                 kernel_size=[7,7],
+                                 strides=[1, 1],
+                                 padding='same',
+                                 activation=None,
+                                 use_bias=False,
+                                 trainable=self._params['TRAINING'],
+                                 name="BottleneckConv2D_plane{0}".format(p))
+            # print x[p].get_shape()
         # The final activation is softmax across the pixels.  It gets applied in the loss function
 #         x = tf.nn.softmax(x)
+
         return x
